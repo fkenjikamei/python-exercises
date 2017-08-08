f8a62b7c9f4863e05dd6ffa6f187860a482a99c6
Fernando Kenji Kamei <fkenjikamei@gmail.com>
2017-08-08 13:55:30 -0300

    Solução sem math
    Solução sem utilizar a biblioteca Math.

--- aula17-3-paginacao.py
+++ aula17-3-paginacao.py
@@ -1,11 +1,12 @@
 #Paginação utilizando o FOR
-import math
-
 lista = [1, 0, 20, 3, 4, 5, 60, 7, 8, 9, 10, 11, 12, 20, 30, 40, 40, 20, 45, 29, 21]
-qtd_itens_pagina = 5
-qtd_paginas = math.ceil(len(lista)/float(qtd_itens_pagina))
+qtd_itens_pagina = 2
+tamanho = len(lista)
 
-print("Tamanho:",len(lista))
+if(tamanho%2!=0):
+	qtd_paginas = (tamanho//qtd_itens_pagina)+1
+else:
+	qtd_paginas = tamanho//qtd_itens_pagina
 
 inicio = 0
 
